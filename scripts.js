4c2c3c824aa22d9471aab479fa7b23a535228fc0
t-rav <tbharmon03@gmail.com>
2021-04-08 08:44:44 -0500

    Recreated grid function.

--- scripts.js
+++ scripts.js
@@ -1,29 +1,14 @@
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+const grid = document.querySelector('.container')
 
-window.addEventListener("load", defaultGrid);
-
-
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
+createGrid = () => {
+    for (let i = 0; i < 256; i++) {
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('mouseover', function(event){
+            event.target.style.backgroundColor = 'black';
+        })
+        container.appendChild(div);
+    }
 }
 
-function makeRows(rowNum) {
-
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-};
-
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
-
-    };
-};
+createGrid();
