710c82a667114fb37250aa37973b691afb262db8
t-rav <tbharmon03@gmail.com>
2021-04-23 16:50:04 -0500

    Finished styling. Created button to switch between black and random colors for mouse pointer.

--- scripts.js
+++ scripts.js
@@ -1,21 +1,21 @@
 const grid = document.querySelector('#container');
 const resetBtn = document.getElementById('reset');
 const gridSizeBtn = document.getElementById('grid-size');
+const colorChangeBtn = document.querySelector('#color');
 let nInput = document.querySelector('.input');
 const maxWidth = 500;
 
 
+
 window.addEventListener("load", createDefaultGrid);
+resetBtn.addEventListener("click", resetBoard);
+colorChangeBtn.addEventListener("click", changeColor);
 
 
 function createDefaultGrid() {
-    /* createGrid(16); */
     createCells(16);
+    grid.addEventListener("mouseover", blackColor);
 }
-
-/* function createGrid(n) {
-    grid.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
-} */
 
 function createCells(n) {
     for (let i = 0; i < n; i++) {
@@ -27,7 +27,6 @@ function createCells(n) {
             row.appendChild(box);
         }
         grid.appendChild(row);
-        grid.addEventListener("mouseover", changeColor);
         let boxes = document.getElementsByClassName("box");
         for (k = 0; k < boxes.length; k++) {
             boxes[k].style.width = maxWidth / n + "px";
@@ -36,10 +35,28 @@ function createCells(n) {
     }
 }
 
-function changeColor(e) {
+function randomColor(e) {
     const color1 = Math.floor(Math.random() * 256);
     const color2 = Math.floor(Math.random() * 256);
     const color3 = Math.floor(Math.random() * 256);
     e.target.style.backgroundColor = `rgb(${color1}, ${color2}, ${color3})`;
+}
+
+function blackColor(e) {
+    e.target.style.backgroundColor = "black";
+}
+
+function changeColor(e) {
+    let changeText = document.querySelector("#color");
+    if (changeText.innerText === "Random") {
+    changeText.innerText = "Black";
+    grid.addEventListener("mouseover", randomColor);
+    } else changeText.innerText = "Random";
+    grid.addEventListener("mouseover", blackColor);
+}
+
+function resetBoard() {
+    console.log("n");
+    grid.addEventListener("mouseover", blackColor);
 }
 
