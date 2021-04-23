e69bf9031c67723d658afb60aa22c772556f7ec8
t-rav <tbharmon03@gmail.com>
2021-04-23 13:25:40 -0500

    Added color, font changes, and other CSS stylings.

--- scripts.js
+++ scripts.js
@@ -2,9 +2,18 @@ const grid = document.querySelector('.container')
 let n = '';
 let area = '';
 
+
+function gridSizeInput() {
+
+}
+
 function createGrid() {
     n = 16;
     area = Math.pow(n, 2);
+
+    if ( n >= 100 ) {
+        alert('Select A Number Less Than 100.');
+    } else {
     for (let i = 0; i < area; i++) {
         const div = document.createElement('div');
         div.classList.add('cell');
@@ -12,6 +21,7 @@ function createGrid() {
             event.target.style.backgroundColor = 'black';
         })
         container.appendChild(div);
+    }
     }
 }
 
